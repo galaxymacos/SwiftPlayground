7a197c28babb16b34419c238f588600311755935
Xun Ruan <galaxymacos@gmail.com>
2021-07-02 19:06:06 +0800

    Review extension protocol

--- SwiftPlayground/MyPlayground.playground/Pages/Days 15.xcplaygroundpage/Contents.swift
+++ SwiftPlayground/MyPlayground.playground/Pages/Days 15.xcplaygroundpage/Contents.swift
@@ -63,3 +63,117 @@ struct Test{
     internal var onlyForYourself: String
     public var forEveryone: String
 }
+
+//
+
+class Album{
+    var name:String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getPerformance(){
+        print("The album \(name) sold well")
+    }
+}
+
+class StudioAlbum: Album{
+    var studio:String
+    
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+    
+    override func getPerformance() {
+        print("The studio album \(name) sold well")
+    }
+}
+
+class LiveAlbum: Album{
+    var location:String
+    
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+    
+    override func getPerformance() {
+        print("The live album \(name) sold well")
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var albums:[Album] = [taylorSwift, fearless, iTunesLive]
+for album in albums{
+    album.getPerformance()
+    if let studioAlbum = album as? StudioAlbum{
+        print(studioAlbum.studio)
+    }
+    else if let liveAlbum = album as? LiveAlbum{
+        print(liveAlbum.location)
+    }
+}
+
+albums = [taylorSwift, fearless]
+for album in albums{
+    let studioAlbum = album as! StudioAlbum
+    print("\(studioAlbum.name)")
+}
+
+// Type casting
+print("--------\n")
+for album in albums as? [StudioAlbum] ?? [StudioAlbum]() {  // Try to convert an album array to a studio album array (every element must be a StudioAlbum in this arr in order for this conversion to work)
+    print(album.studio)
+}
+
+// Convert int to string
+var num = 1
+// This will crash
+//var string = num as! String
+var string = String(num)
+
+// extension
+extension Int{
+    func addOne() -> Int{
+        return self+1
+    }
+    
+    // change itself
+    mutating func addOneToSelf(){
+        self += 1
+    }
+}
+
+// extension protocol
+extension Int{
+    func clamp(high: Int, low: Int)->Int{
+        if self > high {
+            return high
+        }
+        else if self < low{
+            return low
+        }
+        return self
+    }
+}
+
+let bigNum: UInt64 = 5
+5.clamp(high: 10, low: 0)
+
+// BinaryInteger is a protocol which Int, Int32...all agree to
+extension BinaryInteger{
+    func clamp(low: Self, high: Self)->Self{
+        if self > high{
+            return high
+        }
+        else if self < low{
+            return low
+        }
+        return self
+    }
+}
